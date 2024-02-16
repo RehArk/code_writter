14e2946d0ddc761ca9c70a317ece83bfc01fc115
RehArk <contact@vincent-clerc.net>
2024-02-16 22:25:18 +0100

    setup jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: [
+    "./tests/mocks/client.js"
+  ],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  testMatch: [
+    "**/?(*.)+(test).[jt]s?(x)"
+  ]
+};
